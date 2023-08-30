f2b83d883b18b91a2129e84720b9d650563cac8c
LUIZHVA <luizhva02@gmail.com>
2023-08-30 16:04:53 -0300

    30/08/2023

---
+++ projetos-Front-End/introducao-ao-js/aula-5/app.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const funcioarios = [
+    {
+        nome:'Hugo Pereira',
+        cargo: 'Front End',
+        imagem: 'hugo.jpg'
+    },
+    {
+        nome: 'Maria Souza',
+        cargo: 'Designer',
+        Imagem: 'maria.jpg'
+    },
+    {
+        nome: 'João Silva',
+        cargo: ' DBA',
+        Imagem: 'joao.jpg'
+    },
+    {
+        nome: 'Ana Maria',
+        cargo: 'BackEnd',
+        imagem: 'ana.jpg' 
+    }
+
+]
+
+function mostrarCards(funcioarios){
+    const cardContainer = document.getElementById('card-container')
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const imagem = document.createElement('img')
+    imagem.src = `./img/${funcioarios.imagem}`
+    imagem.alt = funcioarios.nome
+
+    const cardDescricao = document.createElement('div')
+    cardDescricao.classList.add('card-descricao')
+
+    const nome = document.createElement('h2')
+    nome.textContent = funcioarios.nome
+
+    const cargo = document.createElement('p')
+    cargo.textContent = funcioarios.cargo
+
+    cardDescricao.replaceChildren(nome, cargo)
+    card.replaceChildren(imagem, cardDescricao)
+    cardContainer.appendChild(card)
+}
+
+funcioarios.forEach(mostrarCards)
