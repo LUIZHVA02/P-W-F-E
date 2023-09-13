c0e7803e6dca7a98ca76783f7fd85324217de52d
LUIZHVA <luizhva02@gmail.com>
2023-09-13 17:25:28 -0300

    13/09/2023

--- projetos-Front-End/introducao-ao-js/aula-5/aula-5v2/app.js
+++ projetos-Front-End/introducao-ao-js/aula-5/aula-5v2/app.js
@@ -1,67 +1,105 @@
 'use strict'
 
-const perfis = [
+const clientes = [
     {
         nome: 'Jerome Bueno',
-        imagem: 'jerome.jpg',
+        imagem: 'jerome.png',
         mensagem: 'Posso realizar o pagamento por...',
         appIcon: 'whatsapp.jpg',
-        contato: 'João Paulo',
+        empregado: 'João Paulo',
         tempo: '30 minutos',
         quantidade: '1'
     },
     {
         nome: 'Daniel Silva',
-        imagem: 'daniel.jpg',
+        imagem: 'daniel.png',
         mensagem: 'Márcia diz: Bom dia, como posso ajudar...',
         appIcon: 'messenger.jpg',
-        contato: 'Márcia',
+        empregado: 'Márcia',
         tempo: '30 minutos',
         quantidade: '1'
     },
     {
         nome: 'Bruno Adorno',
-        imagem: 'bruno.jpg',
+        imagem: 'bruno.png',
         mensagem: 'Bom dia, gostaria de saber quanto...',
         appIcon: 'sms.jpg',
-        contato: 'Joysi',
+        empregado: 'Joysi',
         tempo: '30 minutos',
         quantidade: '1'
     },
     {
         nome: 'Viviane Ribeiro',
-        imagem: 'viviane.jpg',
+        imagem: 'viviane.png',
         mensagem: 'Adorei o atendimento, foi nota 10...',
         appIcon: 'instagram.jpg',
-        contato: 'Geovana',
+        empregado: 'Geovana',
         tempo: '30 minutos',
         quantidade: '1'
     },
     {
         nome: 'Maria Joana',
-        imagem: 'maria.jpg',
+        imagem: 'maria.png',
         mensagem: 'Jhonatan diz: A senhora necessita de mais ...',
         appIcon: 'whatsapp.jpg',
-        contato: 'Jhonatan',
+        empregado: 'Jhonatan',
         tempo: '30 minutos',
         quantidade: '1'
     },
     {
         nome: 'Juliana Pereira',
-        imagem: 'jerome.jpg',
+        imagem: 'jerome.png',
         mensagem: 'Saulo diz: Estarei enviando o comprovante...',
         appIcon: 'instagram.jpg',
-        contato: 'Saulo',
+        empregado: 'Saulo',
         tempo: '30 minutos',
         quantidade: '1'
     },
     {
         nome: 'Henrique Martins',
-        imagem: 'jerome.jpg',
+        imagem: 'jerome.png',
         mensagem: 'Joysi diz: Estarei enviando o comprovante...',
         appIcon: 'whatsapp.jpg',
-        contato: 'Joysi',
+        empregado: 'Joysi',
         tempo: '30 minutos',
         quantidade: '1'
     }
 ]
+
+function mostrarConversas(clientes) {
+    const card = document.getElementById('card')
+
+    const cardContainer = document.getElementById('cardContainer')
+
+    const cardPerfilCliente = document.createElement('div')
+    cardPerfilCliente.classList.add('cardPerfilCliente')
+
+    const clienteIMG = document.createElement('div')
+    clienteIMG.classList.add(clienteIMG)
+
+    const imagem = document.createElement('img')
+    imagem.src = `./img/${clientes.imagem}`
+    imagem.alt = clientes.imagem
+    
+    const nome = document.createElement('h2')
+    nome.textContent = clientes.nome
+    
+    const mensagem = document.createElement('p')
+    mensagem.textContent = clientes.mensagem
+
+    const cardContatoEmpregado = document.createElement('div')
+    cardContatoEmpregado.classList.add('cardContatoEmpregado')
+    
+    const icon = document.createElement('div')
+    icon.classList.add('icon')
+
+    const iconMidia = document.createElement('img')
+    iconMidia.src = `./icons/${clientes.appIcon}`
+    iconMidia.alt = clientes.appIcon
+
+
+    card.replaceChild(cardContainer)
+    cardContainer.replaceChild(cardPerfilCliente)
+    cardPerfilCliente.replaceChildren(clienteIMG, nome, mensagem, cardContatoEmpregado)
+    cardContatoEmpregado.replaceChildren()
+}
