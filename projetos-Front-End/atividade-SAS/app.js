3dc586f742777b57f1448e3e6c48c96c4e190597
LUIZHVA <luizhva02@gmail.com>
2023-10-18 17:25:09 -0300

    18/10/2023

--- projetos-Front-End/atividade-SAS/app.js
+++ projetos-Front-End/atividade-SAS/app.js
@@ -1,105 +1,55 @@
 'use strict'
 
-const id_image = document.getElementById('image_id')
+const infoInput = document.getElementById('ids')
 
-function criarImagem(imagem){
-    const all = document.getElementById('all')
-    const artWorkIMG = document.createElement('div')
-    artWorkIMG.classList.add('artWorkIMG')
-    const tagImg = document.createElement('img')
-    tagImg.src = imagem
-
-    all.appendChild(artWorkIMG)
-    artWorkIMG.appendChild(tagImg)
-}
-
-async function pegarIDImagem(image_id){
-    const url = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`
+async function pegarIDImagem(image_ids) {
+    const url = `https://www.artic.edu/iiif/2/${image_ids}/full/843,/0/default.jpg`
     const response = await fetch(url)
     const imagens = await response.json()
     return imagens.message
 
 }
-
-async function pegarTitulo(title){
-    const url = `https://api.artic.edu/api/v1/artworks/search?q={${title}}&&fields=id,title,artist_display,date_display,image_id,place_of_origin`
+// https://api.artic.edu/api/v1/artworks/search?q=double-portrait-of-fran%C3%A7ois-heurtier-and-his-wife-marie-victoire-jobb%C3%A9-in-profile
+async function pegarTitulo(titles) {
+    const url = `https://api.artic.edu/api/v1/artworks/search?q=${titles}&&fields=id,title,artist_title,date_display,image_id,place_of_origin`
     const response = await fetch(url)
     const title = await response.json()
-    return title.message
+    return title
 }
 
-async function pegarAutor(artist_display){
-    const url = `https://api.artic.edu/api/v1/artworks/search?q={${artist_display}}&&fields=id,title,artist_display,date_display,image_id,place_of_origin`
+async function pegarAutor(artist_titles) {
+    const url = `https://api.artic.edu/api/v1/artworks/search?q=${artist_titles}&&fields=id,title,artist_title,date_display,image_id,place_of_origin`
     const response = await fetch(url)
-    const artist_display = await response.json()
-    return artist_display.message
+    const artist_title = await response.json()
+    return artist_title
 }
 
-async function pegarAno(date_display){
-    const url = `https://api.artic.edu/api/v1/artworks/search?q={${date_display}}&&fields=id,title,artist_display,date_display,image_id,place_of_origin`
+async function pegarAno(date_displays) {
+    const url = `https://api.artic.edu/api/v1/artworks/search?q=${date_displays}&&fields=id,title,artist_title,date_display,image_id,place_of_origin`
     const response = await fetch(url)
     const date_display = await response.json()
-    return date_display.message
+    return date_display
 }
 
-async function pegarID(id){
-    const url = `https://api.artic.edu/api/v1/artworks/${id}`
+async function pegarID(ids) {
+    const url = `https://api.artic.edu/api/v1/artworks/${ids}`
     const response = await fetch(url)
-    const id = await response.json()
-    return id.message
+    const artist_titles = await response.json()
+    return artist_titles
 }
 
-async function preencherCampos(){
-    const image_id = document.getElementById('image_id')
-    const artist_display = document.getElementById('artist_display')
-    const date_display = document.getElementById('date_display')
-    const id = document.getElementById('id')
-    const title = document.getElementById('title')
+async function preencherCamposID() {
+    const image_ids = document.getElementById('image_ids')
+    const artist_title = document.getElementById('artist_titles')
+    const date_display = document.getElementById('date_displays')
+    const title = document.getElementById('titles')
 
-    if(pegarIDImagem(image_id)!=null){
-        const info = await pegarIDImagem(infoInput.value)
+    const info = await pegarID(infoInput.value)
 
-        artist_display.value = info.artist_display
-        date_display.value = info.date_display
-        id.value = info.id
-        title.value = info.title
-
-    } else if (pegarAutor(artist_display)!=null){
-        const info = await pegarAutor(infoInput.value)
-
-        image_id.value = info.image_id
-
-        date_display.value = info.date_display
-        id.value = info.id
-        title.value = info.title
-
-    } else if(pegarAno(date_display)!=null){
-        const info = await pegarAno(infoInput.value)
-        
-        image_id.value = info.image_id
-        artist_display.value = info.artist_display
-        
-        id.value = info.id
-        title.value = info.title
-
-    } else if (pegarID(id)!=null){
-        const info = await pegarID(infoInput.value)
-
-        image_id.value = info.image_id
-        artist_display.value = info.artist_display
-        date_display.value = info.date_display
-        
-        title.value = info.title
-
-    } else if (pegarTitulo(title)!=null){
-        const info = await pegarTitulo(infoInput.value)
-
-        image_id.value = info.image_id
-        artist_display.value = info.artist_display
-        date_display.value = info.date_display
-        id.value = info.id
-
-    }
+        image_ids.value = info.data.image_id
+        artist_title.value = info.data.artist_title
+        date_display.value = info.data.date_display
+        title.value = info.data.title
 
 }
-infoInput.addEventListener('focusout',preencherCampos)
+infoInput.addEventListener('focusout', preencherCamposID)
