aa67a27533810f8457517c2cc77a47bf089bfd71
LUIZHVA <luizhva02@gmail.com>
2024-06-18 17:25:38 -0300

    17:25-18/06/2024

--- projetos-Front-End/atividade-SAS/js/app.js
+++ projetos-Front-End/atividade-SAS/js/app.js
@@ -1,7 +1,20 @@
 export async function pegarObrasArte() {
-    const url = `https://api.artic.edu/api/v1/artworks`
+    const url = `https://api.artic.edu/api/v1/artworks/?fields=id,title,image_id,place_of_origin,artist_display,date_start,date_end,dimensions_detail,exhibition_history,artwork_type_title,department_title,artist_titles,artist_ids,provenance_text,date_display`
     const response = await fetch(url)
     const obrasDeArte = await response.json()
 
     return obrasDeArte
+}
+
+export async function pegarImagens(image_id) {
+
+    let id = image_id
+
+    const imageLink = `https://www.artic.edu/iiif/2/${id}/full/843,/0/default.jpg`
+
+    if (!imageLink) {
+        console.log(imageLink);
+    } else {
+        return imageLink
+    }
 }
--- projetos-Front-End/atividade-SAS/js/main.js
+++ projetos-Front-End/atividade-SAS/js/main.js
@@ -1,58 +1,76 @@
 'use strict'
 
-import { pegarObrasArte } from "./app.js"
+import { pegarImagens, pegarObrasArte } from "./app.js"
 
-async function criaCardObra(obrasDeArte, index) {
-    console.log(obrasDeArte);
-
-    const i = index
-    const image_ids_content = obrasDeArte.id
-    const id = obrasDeArte.id
-    const artist_title_content = obrasDeArte.artist_title
-    const date_display_content = obrasDeArte.date_display
-    const title_content = obrasDeArte.title
+export function criaCardObra(obraDeArte) {
+    const id = obraDeArte.id
+    const image_ids_content = obraDeArte.imageLink
+    const artist_title_content = obraDeArte.artist_title
+    const date_display_content = obraDeArte.date_display
+    const title_content = obraDeArte.title
 
     const card = document.createElement('div')
     card.classList.add('cardObras')
 
     const id_obra = document.createElement('h2')
-    id_obra.classlist.add('id-obra')
+    id_obra.classList.add('id-obra')
     id_obra.textContent = id
 
-    const image_id = document.createElement('h2')
-    image_id.classlist.add('image_ids')
-    image_id.textContent = image_ids_content
+    const image_id = document.createElement('img')
+    image_id.classList.add('image_ids')
+    if(image_ids_content){
+        image_id.src = image_ids_content
+    } else {
+        image_id.src = "../img/png/image_not_found.png"
+    }
     
     const artist_title = document.createElement('h2')
-    artist_title.classlist.add('artist_titles')
+    artist_title.classList.add('artist_titles')
     artist_title.textContent = artist_title_content
     
     const date_display = document.createElement('h2')
-    date_display.classlist.add('date_displays')
+    date_display.classList.add('date_displays')
     date_display.textContent = date_display_content
     
     const title = document.createElement('h2')
-    title.classlist.add('titles')
+    title.classList.add('titles')
     title.textContent = title_content
+
+    
 
     card.append(id_obra, image_id, artist_title, date_display, title)
 
     return card
 }
 
-async function preencherCampos() {
+export async function preencherCampos() {
     const all = document.getElementById('all')
     const obrasArte = await pegarObrasArte()
     let i = 0
+    const arrayArtes = obrasArte.data
+    for(let obra of arrayArtes){
 
+        let image_id =  await pegarImagens(obra.image_id)
 
-    obrasArte.data.forEach(obra => {
+        const id = obra.id
+        const imageLink = image_id
+        const artist_title = obra.artist_title
+        const date_display = obra.date_display
+        const title = obra.title
 
-        const card = criaCardObra(obra, i)
+        let jsonObraDeArte = {
+            id,
+            imageLink,
+            artist_title,
+            date_display,
+            title
+        }
+
+        const card = criaCardObra(jsonObraDeArte)
         all.appendChild(card)
         
         i++
-    });
+    }
 }
 
 window.onload = async () => {
